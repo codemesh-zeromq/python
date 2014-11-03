d5d39032abbce519eb175038917e63b184b2c41d
Robin Kåveland Hansen <rkh@knowit.no>
2014-11-03 16:43:54 +0000

    Colors & nicks.

--- rkh_chat.py
+++ rkh_chat.py
@@ -2,11 +2,18 @@ import multiprocessing
 import zmq
 import time
 import sys
+from fabric import colors
+import random
+
+rainbow = [getattr(colors, color) for color in dir(colors) if not color.startswith("_")]
 
 def ctx():
     return zmq.Context()
 
 port = 7315
+
+def random_color(s):
+    return random.choice(rainbow)(s)
 
 def ip_range(ip_string):
     parts = ip_string.split('.')
@@ -31,13 +38,13 @@ def ui_loop():
     sock = bound_publisher()
     while True:
         line = sys.stdin.readline()
-        sock.send(line.strip())
+        sock.send(('<{}> '.format(sys.argv[1]) + random_color(line.strip())).encode('utf-8'))
 
 def listen_loop():
-    addresses = ip_range(sys.argv[1])
+    addresses = ip_range(sys.argv[2])
     sock = connected_subscriber(addresses)
     while True:
-        print sock.recv().strip()
+        print unicode(sock.recv(), 'utf-8')
 
 if __name__ == '__main__':
     multiprocessing.Process(target=listen_loop).start()
